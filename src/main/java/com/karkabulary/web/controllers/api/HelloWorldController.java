829102bc08b5b444db6bb32ec7caaa3ddbcea4b7
Andrew Dedesko <adedesko@gmail.com>
2020-07-19 23:28:40 -0400

    Add /time endpoint

--- src/main/java/com/karkabulary/web/controllers/api/HelloWorldController.java
+++ src/main/java/com/karkabulary/web/controllers/api/HelloWorldController.java
@@ -3,10 +3,23 @@ package com.karkabulary.web.controllers.api;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 @RestController
 public class HelloWorldController {
     @GetMapping("/")
     public String hello(){
         return "hello world!";
+    }
+
+    @GetMapping("/time")
+    public String time(){
+        TimeZone tz = TimeZone.getTimeZone("UTC");
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'"); // Quoted "Z" to indicate UTC, no timezone offset
+        df.setTimeZone(tz);
+        return df.format(new Date());
     }
 }
