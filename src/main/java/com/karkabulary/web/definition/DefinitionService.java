a09e420bbfb6bd2740600e9e7699a25fe243483e
andrewdedesko <adedesko@gmail.com>
2024-07-22 18:56:43 -0400

    Remove java app

--- src/main/java/com/karkabulary/web/definition/DefinitionService.java
+++
@@ -1,7 +0,0 @@
-package com.karkabulary.web.definition;
-
-import com.google.common.collect.ImmutableList;
-
-public interface DefinitionService {
-    ImmutableList<Definition> getDefinitions();
-}
