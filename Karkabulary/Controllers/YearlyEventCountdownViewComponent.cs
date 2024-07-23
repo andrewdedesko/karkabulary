7ef6dfa525e477f29cf2e37cd9694a4850d170e6
andrewdedesko <adedesko@gmail.com>
2024-07-22 22:14:07 -0400

    Add birthday countdown

---
+++ Karkabulary/Controllers/YearlyEventCountdownViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Karkabulary.Models.Countdown;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Karkabulary.Controllers {
+    public class YearlyEventCountdownViewComponent : ViewComponent {
+        public IViewComponentResult Invoke(string title, int month, int day) {
+            var now = DateTime.Now;
+
+            DateTime nextEvent;
+            if(month == now.Month && day == now.Day) {
+                nextEvent = now;
+            } else if(month > now.Month || (month == now.Month && day > now.Day)) {
+                nextEvent = new DateTime(now.Year, month, day);
+            } else {
+                nextEvent = new DateTime(now.Year + 1, month, day);
+            }
+
+            var timeRemaining = nextEvent - now;
+            var countdownData = new Countdown(title, (int)Math.Floor(timeRemaining.TotalDays), timeRemaining.Hours, timeRemaining.Minutes);
+            return View(countdownData);
+        }
+    }
+}
---
+++ Karkabulary/Models/Countdown/Countdown.cs
@@ -0,0 +1,15 @@
+﻿namespace Karkabulary.Models.Countdown {
+    public class Countdown {
+        public string Title { get; }
+        public int Days { get; }
+        public int Hours { get; }
+        public int Minutes { get; }
+
+        public Countdown(string title, int days, int hours, int minutes) {
+            Title = title;
+            Days = days;
+            Hours = hours;
+            Minutes = minutes;
+        }
+    }
+}
